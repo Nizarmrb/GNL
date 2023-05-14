e23402a2073345215f22a54712232ad67e06b032
Nizar Elmrabet <nelmrabe@c1r6p7.1337.ma>
2023-05-14 19:25:26 +0100

    push

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nelmrabe <nelmrabe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 16:59:28 by nelmrabe          #+#    #+#             */
+/*   Updated: 2023/05/14 16:58:35 by nelmrabe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+/*Includes*/
+
+# include<stdlib.h>
+# include<unistd.h>
+# include<fcntl.h>
+
+/*Functions*/
+
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(char *str, char c);
+char	*before_n(char *str);
+char	*after_n(char *ptr);
+char	*get_next_line(int fd);
+int		read_fun(int read, char **line);
+#endif
